bb8317163d78e1bd30c30f1a59575c6b33e90271
mahmoudruslan <mahmoudahmedruslan@gmail.con>
2025-06-12 01:11:24 +0300

    Options CRUD

---
+++ app/Http/Controllers/Dashboard/AttributeValueController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\DataTables\AttributeValueDataTable;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AttributeValues\StoreAttributeValueRequest;
+use App\Http\Requests\AttributeValues\UpdateAttributeValueRequest;
+use App\Models\Attribute;
+use App\Models\AttributeValue;
+
+class AttributeValueController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(AttributeValueDataTable $dataTable)
+    {
+        $permissions = userAbility(['attribute-values','store-attribute-values', 'update-attribute-values', 'show-attribute-values','delete-attribute-values']);
+        return $dataTable->with('permissions' , $permissions)->render('dashboard.attribute_values.index');
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $attributes = Attribute::all();
+        return view('dashboard.attribute_values.create',compact('attributes'));
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\AttributeValues\StoreAttributeValueRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreAttributeValueRequest $request)
+    {
+        AttributeValue::create($request->validated());
+        return redirect()->route('admin.attribute-values.index')->with([
+            'message' => __('attribute Created successfully.'),
+            'alert-type' => 'success'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Attribute  $attribute
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AttributeValue $attribute)
+    {
+        dd($attribute);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\AttributeValue  $attributeValue
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AttributeValue $attributeValue)
+    {
+        $attributes = Attribute::all();
+        return view('dashboard.attribute_values.edit', compact('attributes','attributeValue'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\AttributeValues\UpdateAttributeValueRequest  $request
+     * @param  \App\Models\AttributeValue  $attributeValue
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateAttributeValueRequest $request, AttributeValue $attributeValue)
+    {
+        $attributeValue->update($request->validated());
+        return redirect()->route('admin.attribute-values.index')->with([
+            'message' => __('attribute Updated successfully.'),
+            'alert-type' => 'success'
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\AttributeValue  $attributeValue
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AttributeValue $attributeValue)
+    {
+        $attributeValue->delete();
+        return redirect()->route('admin.attribute-values.index')->with([
+            'message' => __('attribute Deleted successfully.'),
+            'alert-type' => 'success'
+        ]);
+    }
+}
---
+++ database/seeders/AttributeValueSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Attribute;
+use App\Models\AttributeValue;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AttributeValueSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        AttributeValue::create([
+            'attribute_id' => 1,
+            'value_ar' => 'أحمر',
+            'value_en' => 'red'
+        ]);
+        AttributeValue::create([
+            'attribute_id' => 1,
+            'value_ar' => 'أزرق',
+            'value_en' => 'blue'
+        ]);
+        AttributeValue::create([
+            'attribute_id' => 1,
+            'value_ar' => 'أسود',
+            'value_en' => 'black'
+        ]);
+        AttributeValue::create([
+            'attribute_id' => 2,
+            'value_ar' => 'S',
+            'value_en' => 'S'
+        ]);
+        AttributeValue::create([
+            'attribute_id' => 2,
+            'value_ar' => 'M',
+            'value_en' => 'M'
+        ]);
+        AttributeValue::create([
+            'attribute_id' => 2,
+            'value_ar' => 'L',
+            'value_en' => 'L'
+        ]);
+    }
+}
